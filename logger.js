cb63fe8cfbf213ffa3eb1c5fa94040ae3bb6582e
Anirudh Mukundan <m.anirudh94@gmail.com>
2016-01-23 20:03:08 -0500

    Make insults come from foaas.

--- logger.js
+++ logger.js
@@ -1,7 +1,7 @@
 var logger = exports;
-logger.debugLevel = 'warn';
+var levels = ['ERROR', 'WARN', 'INFO'];
+logger.debugLevel = 'WARN';
 logger.log = function(level, message) {
-    var levels = ['error', 'warn', 'info'];
     if (levels.indexOf(level) >= levels.indexOf(logger.debugLevel) ) {
         if (typeof message !== 'string') {
             message = JSON.stringify(message);
--- server.js
+++ server.js
@@ -1,22 +1,56 @@
 var login = require("facebook-chat-api"),
     prompt = require('prompt'),
     argv = require('yargs').argv,
+    http = require('http'),
+    cheerio = require('cheerio'),
     logger = require('./logger');
 
-logger.debugLevel = 'info';
+logger.debugLevel = 'INFO';
 
 var commands = [
     "\"Olaf help\": get help",
     "\"Olaf hi/hey\": say hello to olaf",
-    "\"Olaf insult <name>\": get olaf to insult <name>"
+    "\"Olaf insult <name>\": olaf to insult <name>",
+    "\"Olaf insult <keyword> <name>\": olaf to special insult <name>"
 ];
 
-// TODO: this is a temporary store of insults
-// will be moved to JSON/DB or use online API  
-var insults = [
-    "Fuck you",
-    "Suck ma snowy dick"
-];
+var get_url = function(params) {
+    var p, url, _i, _len;
+    url = "";
+    for (_i = 0, _len = params.length; _i < _len; _i++) {
+        p = params[_i];
+        url += "/" + (encodeURIComponent(p));
+    }
+    return url;
+};
+
+var fuckoff_request = function(params, callback) {
+    var service_host = "foaas.com";
+    var path;
+    if(params.name == null) {
+        path = get_url([params.keyword, params.from]);
+    } else {
+        path = get_url([params.keyword, params.name, params.from]);
+    }
+    
+    logger.log("INFO", "Requesting insult from: " + service_host + path);
+    var options = {
+        host: service_host,
+        port: 80,
+        path: path,
+        method: 'GET'
+    };
+    return http.request(options, function(res) {
+        res.setEncoding('utf8');
+        return res.on('data', function(data) {
+            data = cheerio.load(data)("h1").text();
+            logger.log("INFO", "Insult response: " + data);
+            return callback(data);
+        });
+    }).on('error', function(e) {
+        return console.error(e.message);
+    }).end();
+};
 
 prompt.override = argv;
 prompt.start();
@@ -45,6 +79,7 @@ prompt.get(['email', {
             switch (event.type) {
                 case "message":
                     var cmdStr = event.body.trim().toLowerCase();
+                    logger.log("INFO", "Command: " + cmdStr);
                     var cmd = cmdStr.split(" ");
                     var res = "";
 
@@ -57,36 +92,52 @@ prompt.get(['email', {
                                     for (var i in commands) {
                                         res += commands[i] + "\n";
                                     }
+                                    api.sendMessage(res, event.threadID);
                                     break;
                                 case "hi":
                                     //fall through
                                 case "hey":
                                     res = "sup " + event.senderName.split(" ")[0] + "?";
+                                    api.sendMessage(res, event.threadID);
                                     break;
                                 case "insult":
-                                    if (cmd.length >= 3) {
-                                        var name = "";
-                                        for (var i = 2; i < cmd.length; i++) {
-                                            name += " " + cmd[i];
+                                    if (cmd.length == 4) {
+                                        var params = {
+                                            keyword: cmd[2],
+                                            name: cmd[3],
+                                            from: event.senderName.split(" ")[0]
+                                        };
+                                        fuckoff_request(params, function(result) {
+                                            res = result;
+                                            api.sendMessage(res, event.threadID);
+                                        });
+                                    } else if (cmd.length >= 3) {
+                                        for (var i = 3; i < cmd.length; i++) {
+                                            cmd[2] += " " + cmd[i];
                                         }
-                                        var rndIndex = Math.floor((Math.random() * 1000) % insults.length);
-                                        var insult = insults[rndIndex];
-                                        res = insult + name;
+                                        var params = {
+                                            keyword: cmd[2],
+                                            name: null,
+                                            from: event.senderName.split(" ")[0]
+                                        };
+                                        fuckoff_request(params, function(result) {
+                                            res = result;
+                                            api.sendMessage(res, event.threadID);
+                                        });
                                     } else {
                                         res = "wtf dude?";
+                                        api.sendMessage(res, event.threadID);
                                     }
                                     break;
                                 default:
                                     res = "I'm sorry, I can't hear you over the sound of how awesome I am";
+                                    api.sendMessage(res, event.threadID);
                             }
                         } else {
                             //unrecognized command
                             res = "wtf do you want " + event.senderName.split(" ")[0] + "?"
+                            api.sendMessage(res, event.threadID);
                         }
-
-                        console.log("command:" + cmd);
-                        console.log("sending:" + res);
-                        api.sendMessage(res, event.threadID);
                     }
 
                     api.markAsRead(event.threadID, function(err) {
@@ -94,7 +145,7 @@ prompt.get(['email', {
                     });
                     break;
                 case "event":
-                    console.log(event);
+                    logger.log("INFO", event);
                     break;
             }
         });
