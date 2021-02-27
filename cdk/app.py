3489c8235595779009df3da4eb1dd10fd4dea970
Anirudh Mukundan <m.anirudh94@gmail.com>
2021-02-27 14:34:04 -0500

    Create blank CDK infra

---
+++ cdk/app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from aws_cdk import core
+
+from lib.infra_stack import InfraStack
+
+
+app = core.App()
+InfraStack(app, "OlafBotInfra", env={'region': 'us-east-1'})
+
+app.synth()
